9dff0370f551cdea213b9bbd6db3d5d553e20f38
Melinda95 <mmdzl@126.com>
2017-11-06 17:05:51 +0800

    tw

---
+++ eight.py
@@ -0,0 +1,17 @@
+b = False
+c = True
+if b:
+    print("b is True")
+elif c:
+    print("c is Ture")
+else:
+    print("Both are False")
+
+b = False
+c = False
+if b:
+    print("b is True")
+elif c:
+    print("c is Ture")
+else:
+    print("Both are False")
